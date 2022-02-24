0e4e6d64fd97729a3aa741610e9920fc965b0299
raheemadamboev <vividricemaryandmax@gmail.com>
2022-02-24 17:32:47 +0500

    added row flexbox and styling

--- App.tsx
+++ App.tsx
@@ -4,10 +4,21 @@ import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 export default function App() {
   return (
     <View style={{ padding: 50 }}>
-      <View>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center"
+        }}
+      >
         <TextInput
           placeholder="Please write your goal here"
-          style={{ borderWidth: 1, borderColor: "black", padding: 10 }}
+          style={{
+            width: "70%",
+            borderWidth: 1,
+            borderColor: "black",
+            padding: 10
+          }}
         />
         <Button title="Add Goal" onPress={() => {}} />
       </View>
