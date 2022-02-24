0c054347d486cbc1aaa6c014b7caa8f5723095e8
raheemadamboev <vividricemaryandmax@gmail.com>
2022-02-24 19:14:32 +0500

    giving life to TextInput and console log result using state and redux

--- App.tsx
+++ App.tsx
@@ -1,29 +1,48 @@
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [goal, setGoal] = useState("");
+
+  const setInput = (input: string) => {
+    setGoal(input);
+  };
+
+  const onAddGoal = () => {
+    console.log(goal)
+  };
+
   return (
-    <View style={{ padding: 50 }}>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
         <TextInput
           placeholder="Please write your goal here"
-          style={{
-            width: "70%",
-            borderWidth: 1,
-            borderColor: "black",
-            padding: 10,
-          }}
+          style={styles.input}
+          onChangeText={setInput}
+          value={goal}
         />
-        <Button title="Add Goal" onPress={() => {}} />
+        <Button title="Add Goal" onPress={onAddGoal} />
       </View>
       <View />
       <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    width: "70%",
+    borderWidth: 1,
+    borderColor: "black",
+    padding: 10,
+  },
+});
